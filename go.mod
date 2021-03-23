219d9c6aa72bc611d9c66219fae5634c79f2de60
Brian Longmore <brianl.ext@gmail.com>
2021-03-23 00:06:26 -1000

    init

---
+++ go.mod
@@ -0,0 +1,10 @@
+module imgclidg/snicker
+
+go 1.16
+
+require (
+	github.com/bwmarrin/discordgo v0.23.2 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2 // indirect
+	golang.org/x/sys v0.0.0-20210320140829-1e4c9ba3b0c4 // indirect
+)
---
+++ main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+var (
+	Token string
+)
+
+func init() {
+	flag.StringVar(&Token, "t", "", "Bot Token")
+	flag.Parse()
+}
+
+func main() {
+	dg, err := discordgo.New("Bot " + Token)
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
+	}
+
+	dg.AddHandler(messageCreate)
+
+	dg.Identify.Intents = discordgo.IntentsGuildMessages
+
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	dg.Close()
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	var (
+		msgHead string
+		arg     string
+		err     error
+	)
+
+	if m.Author.ID == s.State.User.ID || len(m.Content) < 2 {
+		return
+	}
+
+	// using janky command code for now
+
+	msgHead = m.Content[:2]
+
+	if msgHead == "!n" {
+		if m.Content == msgHead {
+			err = errors.New("arg not provided")
+		} else {
+			arg = strings.TrimSpace(m.Content[3:])
+
+			_, err = s.ChannelMessageSend(m.ChannelID, "Nothing can replace "+arg)
+		}
+
+		if err != nil {
+			fmt.Println("failed to send message", err)
+		}
+
+	}
+}
